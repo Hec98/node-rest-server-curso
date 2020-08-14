4ac05180656890bb54a385d2ae2ebbd06df97645
Héctor De La Torre Jiménez Héctor <hector982015@gmail.com>
2020-08-13 20:06:38 -0500

    Avance hasta video 98 del curso de Node

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -35,9 +35,22 @@ app.post('/usuario', (req, res) => {
 
 app.put('/usuario/:id', (req, res) => {
     let id = req.params.id;
-    res.json({
-        id
+    let body = req.body;
+
+    Usuario.findByIdAndUpdate(id, body, {new: true}, (err, usuarioDB) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+        res.json({
+            ok: true,
+            usuario: usuarioDB
+        });
     });
+
+    
 });
 
 app.delete('/usuario', (req, res) => {
