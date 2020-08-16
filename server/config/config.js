cad697e0b9ed3a1776e0b49b61341947286c80f2
Héctor De La Torre Jiménez Héctor <hector982015@gmail.com>
2020-08-16 16:19:41 -0500

    Variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -7,6 +7,6 @@ process.env.NODE_ENV  = process.env.NODE_ENV || 'dev';
 // Base de datos
 let urlDB;
 if (process.env.NODE_ENV === 'dev') urlDB = 'mongodb://localhost:27017/cafe';
-else urlDB = 'mongodb+srv://jluis:zmlo5jJ8QFYHFHnq@cluster0.gr6fs.mongodb.net/cafe';
+else urlDB = process.env.MONGO_URI;
 
 process.env.URLDB = urlDB;
