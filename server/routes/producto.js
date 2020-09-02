7b1e7712b3fea4481f61caf2274ea703c43bad12
Héctor De La Torre Jiménez Héctor <hector982015@gmail.com>
2020-09-01 19:15:04 -0500

    Realizar búsquedas en base de datos

--- server/routes/producto.js
+++ server/routes/producto.js
@@ -10,6 +10,8 @@ app.get('/productos', verificaToken, (req, res) => {
     // populate: usuario y categoria
     // paginado
     Producto.find({disponible: true})
+        .populate('usuario', 'nombre email')
+        .populate('categoria', 'descripcion')
         .exec((err, productoDB) => {
             if (err) return res.status(500).json({ ok: false, err });
             Producto.countDocuments({disponible: true})
@@ -25,11 +27,28 @@ app.get('/productos', verificaToken, (req, res) => {
 app.get('/productos/:id', verificaToken, (req, res) => {
     // populate: usuario y categoria
     let id = req.params.id;
-    Producto.findById(id, (err, productoDB) => {
-        if (err) return res.status(500).json({ ok: false, err });
-        if (!productoDB) res.status(404).json({ ok: false, err: { message: 'Producto no encontrado' } });
-        res.status(201).json({ ok: true, productoDB });
-    });
+    Producto.findById(id)
+        .populate('usuario', 'nombre email')
+        .populate('categoria', 'descripcion')
+        .exec((err, productoDB) => {
+            if (err) return res.status(500).json({ ok: false, err });
+            if (!productoDB) res.status(404).json({ ok: false, err: { message: 'Producto no encontrado' } });
+            res.status(201).json({ ok: true, productoDB });
+        });
+});
+
+// Buscar productos
+app.get('/productos/buscar/:termino', verificaToken, (req, res) => {
+    let termino = req.params.termino;
+    let regex = new RegExp(termino, 'i');
+    Producto.find({nombre: regex})
+        .populate('usuario', 'nombre email')
+        .populate('categoria', 'descripcion')
+        .exec((err, productoDB) => {
+            if (err) return res.status(500).json({ ok: false, err });
+            if (!productoDB) res.status(404).json({ ok: false, err: { message: 'Producto no encontrado' } });
+            res.status(201).json({ ok: true, productoDB });
+        });
 });
 
 // Crear un producto
